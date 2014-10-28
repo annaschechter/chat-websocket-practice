f980b0d87a973503cae5aa74e70263a24088f884
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-10-28 18:33:05 +0000

    Displaying a user connected/disconnected

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var server = express();
 var http = require('http').createServer(server);
+var io = require('socket.io')(http);
 expressLayouts = require('express-ejs-layouts')
 
 server.set('views', __dirname + '/views');
@@ -14,7 +15,13 @@ server.get('/', function(req, res) {
 	res.render('index')
 });
 
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('a user disconnected');
+	});
+});
 
 http.listen(3000, function() {
 	console.log("Listening to the port: 3000")
-})
+});
