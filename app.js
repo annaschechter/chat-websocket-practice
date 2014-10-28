e78571b80cbae8a84414d88b28de26282429bf9f
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-10-28 18:15:59 +0000

    Linked public folder and created message window

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var server = express();
 var http = require('http').createServer(server);
+expressLayouts = require('express-ejs-layouts')
 
-server.get('/', function() {
-	console.log('Hello world!')
+server.set('views', __dirname + '/views');
+server.set('view engine', 'ejs');
+server.use(express.static(__dirname + '/public'));
+server.use(expressLayouts)
+server.set('layout', 'layout')
+
+
+server.get('/', function(req, res) {
+	res.render('index')
 });
+
 
 http.listen(3000, function() {
 	console.log("Listening to the port: 3000")
